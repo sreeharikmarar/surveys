6677091a6674e84c0972b965cc54b481b03b6911
sreeharikmarar <sreeharikmarar@gmail.com>
2014-09-29 16:38:49 +0530

    user spec modified

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,8 @@
 
 FactoryGirl.define do
   factory :user do 
-    # u.username {|u| "#{Faker::Name.name}".gsub(" ","").downcase.strip }
-    # u.email {|u| "#{u.username}@example.com" }
-    # u.password "password"
-
-  sequence(:username) {|n| "User Name #{n}".gsub(" ","").downcase.strip }
-  sequence(:email) {|n| "user#{n}@test.com" }
-  sequence(:password) {|n| "password" }
+	sequence(:username) {|n| "User Name #{n}".gsub(" ","").downcase.strip }
+  	sequence(:email) {|n| "user#{n}@test.com" }
+     sequence(:password) {|n| "password" }
   end
 end
